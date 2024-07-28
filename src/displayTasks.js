c1ff0480ae2180846a98df0c15b18815617e502f
Marcus Leong <marcusleongjx@gmail.com>
2024-07-28 19:00:28 +0800

    Add styling for general display and buttons

--- src/displayTasks.js
+++ src/displayTasks.js
@@ -1,6 +1,6 @@
 export default function displayTasks(allTodoArr, index) {
-    const body = document.querySelector('body');
+    const display = document.querySelector('#display');
     const taskDisplay = document.createElement('p');
     taskDisplay.innerHTML = allTodoArr[index].title;
-    body.appendChild(taskDisplay);
+    display.appendChild(taskDisplay);
 }
--- src/filterSelection.js
+++ src/filterSelection.js
@@ -2,7 +2,7 @@ import displayTasks from './displayTasks.js';
 
 export default function selectFilter(allTodoArr, filter) {
     let indexArr = [];
-    const body = document.querySelector('body');
+    const display = document.querySelector('#display');
     //check for tasks due today and return array of indexes of these tasks
     if (filter.substring(0, filter.indexOf(':')) == 'dueDate') {
         allTodoArr.forEach((todo, index) => {
@@ -14,7 +14,7 @@ export default function selectFilter(allTodoArr, filter) {
         if (indexArr.length == 0) {
             const nullTodayDisplay = document.createElement('p');
             nullTodayDisplay.innerHTML = 'No tasks due today';
-            body.appendChild(nullTodayDisplay);
+            display.appendChild(nullTodayDisplay);
         //display tasks which match filter of dueDate:today
         } else {
             indexArr.forEach((i) => {
@@ -32,7 +32,7 @@ export default function selectFilter(allTodoArr, filter) {
         if (indexArr.length == 0) {
             const nullProjTasksDisplay = document.createElement('p');
             nullProjTasksDisplay.innerHTML = 'No tasks under this project';
-            body.appendChild(nullProjTasksDisplay);
+            display.appendChild(nullProjTasksDisplay);
         //display tasks which match filter of project selected
         } else {
             indexArr.forEach((i) => {
