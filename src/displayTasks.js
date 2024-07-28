9549d1787f404a14a98b47fd55f2672fa1ed78d7
Marcus Leong <marcusleongjx@gmail.com>
2024-07-28 20:19:49 +0800

    Amend logic for tasks added and add styling

--- src/displayTasks.js
+++ src/displayTasks.js
@@ -1,6 +1,20 @@
 export default function displayTasks(allTodoArr, index) {
     const display = document.querySelector('#display');
-    const taskDisplay = document.createElement('p');
-    taskDisplay.innerHTML = allTodoArr[index].title;
+    const taskDisplay = document.createElement('div');
+    const titleDisplay = document.createElement('p');
+    const descDisplay = document.createElement('p');
+    const duedateDisplay = document.createElement('p');
+    const projectDisplay = document.createElement('p');
+    const priorityDisplay = document.createElement('p');
+    titleDisplay.innerHTML = allTodoArr[index].title;
+    taskDisplay.appendChild(titleDisplay);
+    duedateDisplay.innerHTML = allTodoArr[index].dueDate;
+    taskDisplay.appendChild(duedateDisplay);
+    projectDisplay.innerHTML = allTodoArr[index].project;
+    taskDisplay.appendChild(projectDisplay);
+    priorityDisplay.innerHTML = allTodoArr[index].priority;
+    taskDisplay.appendChild(priorityDisplay);
+    descDisplay.innerHTML = allTodoArr[index].description;
+    taskDisplay.appendChild(descDisplay);
     display.appendChild(taskDisplay);
 }
