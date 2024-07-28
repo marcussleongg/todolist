69ab576212eb6c372433001e766e99bde9bb6fd0
Marcus Leong <marcusleongjx@gmail.com>
2024-07-28 15:51:33 +0800

    Amend logic for checking tasks due today using date-fns

--- src/clickOnFilter.js
+++ src/clickOnFilter.js
@@ -1,10 +1,9 @@
 import selectFilter from './filterSelection.js';
+import { format } from "date-fns";
 
 export default function clickOnFilter(allTodoArr) {
     const dueToday = document.querySelector('#dueToday');
     dueToday.addEventListener('click', () => {
-        selectFilter(allTodoArr, 'dueDate:today');
+        selectFilter(allTodoArr, `dueDate:${format(new Date(), "yyyy-MM-dd")}`);
     })
 }
-
-// how am i going to add event listeners to the display of projects, when the projects are changing/can be added
