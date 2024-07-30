900c269b3e8980cf9d547ec37422f575695ad57f
Marcus Leong <marcusleongjx@gmail.com>
2024-07-30 17:10:15 +0800

    Fix functionality of project dropdown options

--- src/prepForm.js
+++ src/prepForm.js
@@ -2,6 +2,10 @@ export default function prepForm(projDropdown, allProjects) {
     while (projDropdown.lastElementChild) {
         projDropdown.removeChild(projDropdown.lastElementChild);
     }
+    const noProj = document.createElement('option');
+    noProj.value = '-';
+    noProj.innerHTML = '-';
+    projDropdown.appendChild(noProj);
     allProjects.forEach((project) => {
         const option = document.createElement('option');
         option.value = project.title;
