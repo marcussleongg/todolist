0f1530491b5224ce1d360c65364535a3573e47de
Marcus Leong <marcusleongjx@gmail.com>
2024-08-01 21:09:39 +0800

    Fix functionality of delete project button

--- src/deleteProj.js
+++ src/deleteProj.js
@@ -1,15 +1,16 @@
 import deleteTask from './deleteTask.js'
 
 export default function deleteProj(proj, allProjsArr, allTodoArr) {
-    console.log(allTodoArr.length);
+    let i = allProjsArr.map(e => e.title).indexOf(proj);
+    allProjsArr.splice(i, 1);
     let indexArr = [];
     allTodoArr.forEach((task) => {
         if (task['project'] == proj) {
-            indexArr.push(allTodoArr.indexOf(task));
+            indexArr.push(allTodoArr[allTodoArr.indexOf(task)]['title']);
         }
     })
+    console.log(indexArr);
     indexArr.forEach((index) => {
-        deleteTask(allTodoArr[index], allTodoArr);
+        deleteTask(index, allTodoArr);
     })
-    allProjsArr.splice(allProjsArr.indexOf(proj), 1);
 }
--- src/displayProj.js
+++ src/displayProj.js
@@ -1,17 +1,17 @@
 import selectFilter from "./filterSelection.js";
 
-export default function displayProj(projects, allTodoArr, completedTasksArr) {
+export default function displayProj(allProjsArr, allTodoArr, completedTasksArr) {
     const projDiv = document.querySelector('#projects');
     while (projDiv.lastElementChild) {
         projDiv.removeChild(projDiv.lastElementChild);
     }
-    projects.forEach((project) => {
+    allProjsArr.forEach((project) => {
         const projDisplay = document.createElement('p');
         projDisplay.innerHTML = project.title;
         //projDisplay.setAttribute('id', project.title);
         let filterStr = `project:${project.title}`;
         projDisplay.addEventListener('click', () => {
-            selectFilter(allTodoArr, filterStr, projects, completedTasksArr);
+            selectFilter(allTodoArr, filterStr, allProjsArr, completedTasksArr);
         })
         projDiv.appendChild(projDisplay);
     })
