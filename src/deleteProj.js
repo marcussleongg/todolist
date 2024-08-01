6aeca8b49b005bad1a10b5380dc8d477c618c926
Marcus Leong <marcusleongjx@gmail.com>
2024-08-01 21:13:26 +0800

    Clean up code

--- src/deleteProj.js
+++ src/deleteProj.js
@@ -3,14 +3,13 @@ import deleteTask from './deleteTask.js'
 export default function deleteProj(proj, allProjsArr, allTodoArr) {
     let i = allProjsArr.map(e => e.title).indexOf(proj);
     allProjsArr.splice(i, 1);
-    let indexArr = [];
+    let toDeleteArr = [];
     allTodoArr.forEach((task) => {
         if (task['project'] == proj) {
-            indexArr.push(allTodoArr[allTodoArr.indexOf(task)]['title']);
+            toDeleteArr.push(allTodoArr[allTodoArr.indexOf(task)]['title']);
         }
     })
-    console.log(indexArr);
-    indexArr.forEach((index) => {
-        deleteTask(index, allTodoArr);
+    toDeleteArr.forEach((toDeleteTask) => {
+        deleteTask(toDeleteTask, allTodoArr);
     })
 }
