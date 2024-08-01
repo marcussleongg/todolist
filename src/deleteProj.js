833a6a5030a7b312700f72d89a159cd1e9aaaf5f
Marcus Leong <marcusleongjx@gmail.com>
2024-08-01 21:22:26 +0800

    Add functionality of removing projects from local storage

--- src/deleteProj.js
+++ src/deleteProj.js
@@ -3,6 +3,7 @@ import deleteTask from './deleteTask.js'
 export default function deleteProj(proj, allProjsArr, allTodoArr) {
     let i = allProjsArr.map(e => e.title).indexOf(proj);
     allProjsArr.splice(i, 1);
+    localStorage.removeItem(proj)
     let toDeleteArr = [];
     allTodoArr.forEach((task) => {
         if (task['project'] == proj) {
